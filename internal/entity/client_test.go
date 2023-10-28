63272469d55a821ddfb655a27529343f369b52fc
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-10-28 02:34:00 -0300

    refactor: add account in client entity

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -34,3 +34,11 @@ func TestUpdateClientWithIvalidArgs(t *testing.T) {
 	err := client.Update("", "j@j.com")
 	assert.Error(t, err, "name is required")
 }
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
