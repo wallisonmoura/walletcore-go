b81694a6bc98c7aa720f5148af8c651e7cb561e1
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-10-29 01:55:05 -0300

    feat: add clientDB testing suites

---
+++ internal/database/client_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type ClientDBTestSuite struct {
+	suite.Suite
+	db       *sql.DB
+	clientDB *ClientDB
+}
+
+func (s *ClientDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("Create table clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	s.clientDB = NewClientDB(db)
+}
+
+func (s *ClientDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE clients")
+}
+
+func TestClientDBTestSuite(t *testing.T) {
+	suite.Run(t, new(ClientDBTestSuite))
+}
